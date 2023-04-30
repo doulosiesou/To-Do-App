7f3ab09eb94d556426884d865c490e02a769f4c0
“doulosiesou” <95942272+doulosiesou@users.noreply.github.com>
2023-04-30 10:44:55 -0600

    Initial Layout Complete
    Add header, nav panel, and project/task display area

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import modules
+
+// import handlers for style sheets and images
+import './style.css';
+
+function component() {
+
+    console.log('component function running');
+}
+
+document.body.appendChild(component());
