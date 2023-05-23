a4466f94af6abd7dba7101420cae80ca8779e91a
“doulosiesou” <95942272+doulosiesou@users.noreply.github.com>
2023-05-23 13:31:28 -0600

    Create button to strikeout completed task

--- src/modules/populateTaskList.js
+++ src/modules/populateTaskList.js
@@ -41,13 +41,7 @@ export function populateTaskList(pname){
         let taskRow = document.createElement('tr');
         taskRow.className = 'task-row'
 
-        // create checkboxes
-        let checkBox = document.createElement("INPUT"); 
-        checkBox.setAttribute("type", "checkbox");
-        checkBox.className = 'checkbox';
-        checkBox.id = `cb${indx}`
-        // checkBoxContainer.appendChild(checkBox);
-
+               
         // create task rows and append each to taskTableBody
         let c0 = document.createElement("td");
         let c1 = document.createElement("td");
@@ -56,31 +50,46 @@ export function populateTaskList(pname){
         let c4 = document.createElement("td");
         let c5 = document.createElement("td");
 
-        c0.appendChild(checkBox);
+        
         c1.innerText = pname;
         c2.innerText = task.desc;
         c3.innerText = task.dueDate;
         c4.innerText = task.priority;
 
-        let btn = document.createElement('input');
-        btn.type = 'button';
-        btn.value = 'x'
-        btn.id = `db${indx}`;
-        btn.className = 'task-delete-button';
-        btn.addEventListener("click", function(){
+        let btn2 = document.createElement('input');
+        btn2.type = 'button';
+        btn2.value = 'Delete'
+        btn2.id = `db${indx}`;
+        btn2.className = 'task-delete-button';
+        btn2.addEventListener("click", function(){
             let userInput = prompt(`Are you sure you want to delete this task: ${task.desc}? Answer y/n`);
             if(userInput.toLowerCase === 'n') {
                 alert('You entered no to delete this task');
                 } else {
-                    indx = btn.id.split('-')[1];
+                    indx = btn2.id.split('-')[1];
                     prjTaskArray.splice(indx, 1);
                     localStorage.removeItem(pname);
                     localStorage.setItem(pname, JSON.stringify(prjTaskArray));
                 };
             }
         )
-        c5.appendChild(btn);
+        c5.appendChild(btn2);
 
+        // add a button that strikes out text in row for completed
+        let btn1 = document.createElement('input');
+        btn1.type = 'button';
+        btn1.value = 'Completed'
+        btn1.id = `db${indx}`;
+        btn1.className = 'task-delete-button';
+        btn1.addEventListener("click", function (){
+                                c1.className = 'strikeout';
+                                c2.className = 'strikeout';
+                                c3.className = 'strikeout';
+                                c4.className = 'strikeout';
+                                
+                                });
+        c0.appendChild(btn1);
+        
         taskRow.appendChild(c0);
         taskRow.appendChild(c1);
         taskRow.appendChild(c2);
