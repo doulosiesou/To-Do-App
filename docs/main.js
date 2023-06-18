12485ffb32f7fcc8e3fb6f26b29681bff5267f55
“doulosiesou” <95942272+doulosiesou@users.noreply.github.com>
2023-06-18 10:56:45 -0600

    Set date field with date picker
    Adjust width of project and desc
      fields

--- docs/main.js
+++
@@ -1,1 +0,0 @@
-document.body.appendChild(function(){const e=document.createElement("div"),n=document.createElement("h1");return n.textContent="Building To Do App",e.appendChild(n),e}());
--- src/modules/createTaskForm.js
+++ src/modules/createTaskForm.js
@@ -5,7 +5,7 @@ export function createTaskForm() {
   addDiv.innerHTML = `<form action="#" method="post">
             <input type="text" class = "task-form-item" id = "task-project" placeholder="project">
             <input type="text" class = "task-form-item" id = "task-desc" placeholder="description">
-            <input type="text" class = "task-form-item" id = "task-dueDate" placeholder="MM-dd-yyyy due date">
+            <input type="date" class = "task-form-item" id = "task-dueDate" placeholder="MM-dd-yyyy due date">
             <input type="text" class = "task-form-item" id = "task-priority" placeholder="priority">
         </form>`;
 
